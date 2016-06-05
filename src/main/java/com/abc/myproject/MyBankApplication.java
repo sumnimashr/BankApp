f45cf0c381ee575fd1de69d7feba5555b79781b7
sumnimashr <sumnimaa.kc@gmail.com>
2016-06-05 11:33:58 -0500

    initial commit

---
+++ src/main/java/com/abc/myproject/MyBankApplication.java
@@ -0,0 +1,12 @@
+package com.abc.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBankApplication.class, args);
+	}
+}
